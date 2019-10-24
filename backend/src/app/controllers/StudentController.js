7a280f6b66aab582add5a21a6995f013984d59ef
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-24 15:37:17 -0300

    Continuando desafio: melhorias no StudentController

--- backend/src/app/controllers/StudentController.js
+++ backend/src/app/controllers/StudentController.js
@@ -1,83 +1,104 @@
 import * as Yup from 'yup';
+import { isAfter, parseISO } from 'date-fns';
+
 import Student from '../models/Student';
 
 class StudentController {
-  
+  async index(req, res) {
+    const { page = 1, quantity = 20 } = req.params;
+
+    const students = await Student.findAll({
+      limit: quantity,
+      offset: (page - 1) * quantity,
+    });
+
+    return res.json(students);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const student = await Student.findByPk(id);
+
+    return res.json(student);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string()
-        .email()
-        .required(),
-      age: Yup.number()
-        .integer()
-        .positive()
-        .required(),
-      weight: Yup.number()
-        .positive()
-        .required(),
-      height: Yup.number()
-        .positive()
-        .required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
-    }
-
-    const studentExists = await Student.findOne({
-      where: { email: req.body.email },
-    });
-    if (studentExists) {
-      return res.status(400).json({ error: 'Student alredy exists' });
-    }
-    const { id, name, email, age, weight, height } = await Student.create(
-      req.body
-    );
-    return res.json({
-      id,
-      name,
-      email,
-      age,
-      weight,
-      height,
-    });
-  }
-
-  async update(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string(),
-      email: Yup.string()
-        .email()
-        .required(),
-      age: Yup.number()
-        .integer()
-        .positive(),
-      weight: Yup.number().positive(),
-      height: Yup.number().positive(),
+      email: Yup.string().required(),
+      age: Yup.date().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation failed' });
     }
 
-    const student = await Student.findOne({ where: { email: req.body.email } });
-    if (!student) {
-      return res.status(401).json({ error: 'Student not found' });
+    const { email, age } = req.body;
+
+    if (isAfter(parseISO(age), new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You age cannot be after current date' });
     }
 
-    const { id, name, email, age, weight, height } = await student.update(
-      req.body
-    );
+    const studentExists = await Student.findOne({ where: { email } });
 
-    return res.json({
-      id,
-      name,
-      email,
-      age,
-      weight,
-      height,
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      age: Yup.date().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { id } = req.params;
+    const { name, email, age, weight, height } = req.body;
+
+    if (isAfter(parseISO(age), new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'age date can not be after current date' });
+    }
+
+    const student = await Student.findByPk(id);
+
+    if (student.email !== email) {
+      const studentExists = await Student.findOne({ where: { email } });
+
+      if (studentExists) {
+        return res.status(401).json({ error: 'Email is already in use' });
+      }
+    }
+
+    await student.update({ name, email, age, weight, height });
+    await student.save();
+
+    return res.json(student);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await Student.destroy({ where: { id } });
+
+    return res.send();
   }
 }
 
